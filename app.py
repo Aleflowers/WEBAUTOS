72dc3762e4a133e4e9156a314ed52c81fabbb54b
Aleflowers <chavezfloresale@gmail.com>
2024-11-14 09:28:38 -0600

    Create app.py
    Codigo de python para webautos

---
+++ app.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# Leer el archivo CSV
+df_vehiculos = pd.read_csv('C:\Users\achav\Documents\proyectos\WEBAUTOS\vehicles_us.csv')
+
+# Mostrar las primeras filas del DataFrame
+print(df_vehiculos.head())
